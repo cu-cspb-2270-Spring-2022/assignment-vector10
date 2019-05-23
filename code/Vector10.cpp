66d9d302264a744389ceb22761ac075aa23c7ff0
siag7072 <siag7072@colorado.edu>
2019-05-23 02:16:33 +0000

    updated homework

--- code/Vector10.cpp
+++ code/Vector10.cpp
@@ -1,14 +1,29 @@
 #include "Vector10.h"
 
 Vector10::Vector10(){
-    for(int ii=0; ii<10; ii++){
-        arr[ii] = 0;
-    }
+    // YOUR CODE HERE
 }
 
 Vector10::~Vector10(){
+    // YOUR CODE HERE
 }
 
 int Vector10::ValueAt(int index){
-    return arr[index];
+    // YOUR CODE HERE
+}
+
+bool Vector10::PushBack(int value){
+    // YOUR CODE HERE
+}
+
+int Vector10::CountEmpty(){
+    // YOUR CODE HERE
+}
+
+bool Vector10::Search(int value){
+    // YOUR CODE HERE
+}
+
+bool Vector10::Remove(int index){
+    // YOUR CODE HERE
 }
--- code/Vector10.h
+++ code/Vector10.h
@@ -3,12 +3,34 @@
 
 class Vector10 {
 public:
+    // constructor
     Vector10();
+
+    //deconstructor
     ~Vector10();
+    
+    // return value at index of array
     int ValueAt(int index);
+
+    // puts value in the last empty spot of array
+    bool PushBack(int value);
+
+    // this member function returns number of empty spaces left in the array
+    int CountEmpty();
+    
+    // this function searches the value in the array and returns if value exists
+    // and returns false otherwise
+    bool Search(int value);
+
+    // this function removes a value from array. (removal process includes 
+    // shifting valid member on its right hand side to the left by one index)
+    bool Remove(int index);
+
+    // add more public member variables and member functions here if you need
 
 private:
     int arr[10];
+    // add more private member variables and member functions here if you need
 };
 
 #endif  // VECTOR10_H__
